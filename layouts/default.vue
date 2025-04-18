d53c4a3d842ea1b1f7e8a909d65428d8fd00670a
Amanda Santos <amandasf0200@gmail.com>
2025-04-18 19:50:30 -0300

    feat: add home page

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="layout-container">
+    <main class="h-full mt-16">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.layout-container {
+  height: calc(100vh - 4rem);
+  background-color: var(--ui-bg);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <UContainer class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-4xl font-bold">Nuxt Chat</h1>
+    <UButton to="/chat" class="mt-4">Start Chat</UButton>
+  </UContainer>
+</template>
